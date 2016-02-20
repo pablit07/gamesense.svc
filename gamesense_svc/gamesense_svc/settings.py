b285a35d1947a8278331042e773f71848887c0b4
Paul Kohlhoff <paul.kohlhoff@gmail.com>
2016-02-19 18:36:22 -0700

    Make revisions for AWS deploy. Copied settings from other project. Move API app to correct location. Copied .ebextensions from other project.

--- gamesense_svc/gamesense_svc/settings.py
+++ gamesense_svc/gamesense_svc/settings.py
@@ -74,12 +74,28 @@ WSGI_APPLICATION = 'gamesense_svc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+       'default': {
+             'ENGINE':'django.db.backends.postgresql_psycopg2',
+             'NAME': os.environ['RDS_DB_NAME'],
+             'USER': os.environ['RDS_USERNAME'],
+             'PASSWORD': os.environ['RDS_PASSWORD'],
+             'HOST': os.environ['RDS_HOSTNAME'],
+             'PORT': os.environ['RDS_PORT'],
+             }
+          }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'iotd',
+            'USER': '',
+            'PASSWORD': '',
+            'HOST' : 'localhost',
+            'PORT' : '5432',
+        }
+    } 
 
 
 # Password validation
@@ -118,4 +134,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
